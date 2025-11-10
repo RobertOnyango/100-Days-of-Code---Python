7322c1bc9e01a676bb6e7d9043e0d6d713f9a3fc
RobertOnyango <45780414+RobertOnyango@users.noreply.github.com>
2025-11-10 16:13:32 +0300

    Day 10

--- Day 10 - Functions with Outputs/leap_year.py
+++ Day 10 - Functions with Outputs/leap_year.py
@@ -1,7 +1,7 @@
 # A function that indicates whether the year is a Leap Year
 def is_leap_year(year):
-    # Check if year is divisible by 4, no remainder = Leap AND
-    # Check if it is divisible by 100, remainder = Leap
+    # Check if year is divisible by 4, if there's no remainder => Leap AND check if it is divisible by 100, if there's a remainder => Leap. 
+    # The condition above checks if the number is both fully divisible by 4 and not fully divisible by 100
     # Check if it is divisible by 400, no remainder = Leap
     if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
         return True
