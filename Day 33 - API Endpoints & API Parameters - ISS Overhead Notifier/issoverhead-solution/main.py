4f2f5c008ca1ea82c9607e7f4c270d82910cde2b
Robert <robertmark94@gmail.com>
2026-02-06 20:11:44 +0300

    Day 33

--- Day 33 - API Endpoints & API Parameters - ISS Overhead Notifier/issoverhead-solution/main.py
+++ Day 33 - API Endpoints & API Parameters - ISS Overhead Notifier/issoverhead-solution/main.py
@@ -5,10 +5,11 @@ import time
 
 MY_EMAIL = "___YOUR_EMAIL_HERE____"
 MY_PASSWORD = "___YOUR_PASSWORD_HERE___"
-MY_LAT = 51.507351 # Your latitude
-MY_LONG = -0.127758 # Your longitude
+MY_LAT = -1.292066 # Your latitude
+MY_LONG = 36.821945 # Your longitude
 
 
+# Function that gets the current latitude and longitude position of the iss_satellite, and finds its proximity uses to my current latitude and longitude positions. Returns true if both are (-5 <= x <= +5)
 def is_iss_overhead():
     response = requests.get(url="http://api.open-notify.org/iss-now.json")
     response.raise_for_status()
@@ -22,6 +23,7 @@ def is_iss_overhead():
         return True
 
 
+# Function that takes the current time (only the hour) and figures out if it is night time in my current loaction
 def is_night():
     parameters = {
         "lat": MY_LAT,
@@ -31,17 +33,22 @@ def is_night():
     response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
     response.raise_for_status()
     data = response.json()
+    # Here we only pick the hour value of the projected sunrise and sunset times of my location
     sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
     sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
+    # Pick the hour value for now()
     time_now = datetime.now().hour
 
+    # Find out if its dark using the time now and sunrise, sunset times
     if time_now >= sunset or time_now <= sunrise:
         return True
 
-
+# Loop that will always run since it is always true
 while True:
+    # Run the code succeeding the time.sleep() every 60 seconds
     time.sleep(60)
+    # Confirm if iss_is_overhead and it is_night.
     if is_iss_overhead() and is_night():
         connection = smtplib.SMTP("__YOUR_SMTP_ADDRESS_HERE___")
         connection.starttls()
