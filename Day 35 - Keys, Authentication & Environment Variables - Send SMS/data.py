62e8d3cdc2a790cddfe7cdf62194ee3a22cfb643
Robert <robertmark94@gmail.com>
2026-02-19 20:07:24 +0300

    Day 35

---
+++ Day 35 - Keys, Authentication & Environment Variables - Send SMS/data.py
@@ -0,0 +1,164 @@
+{
+  "cod": "200",
+  "message": 0,
+  "cnt": 4,
+  "list": [
+    {
+      "dt": 1771524000,
+      "main": {
+        "temp": 298.24,
+        "feels_like": 297.74,
+        "temp_min": 294.39,
+        "temp_max": 298.24,
+        "pressure": 1010,
+        "sea_level": 1010,
+        "grnd_level": 838,
+        "humidity": 36,
+        "temp_kf": 3.85
+      },
+      "weather": [
+        {
+          "id": 803,
+          "main": "Clouds",
+          "description": "broken clouds",
+          "icon": "04n"
+        }
+      ],
+      "clouds": {
+        "all": 51
+      },
+      "wind": {
+        "speed": 3.74,
+        "deg": 72,
+        "gust": 9.39
+      },
+      "visibility": 10000,
+      "pop": 0,
+      "sys": {
+        "pod": "n"
+      },
+      "dt_txt": "2026-02-19 18:00:00"
+    },
+    {
+      "dt": 1771534800,
+      "main": {
+        "temp": 295.74,
+        "feels_like": 295.28,
+        "temp_min": 293.52,
+        "temp_max": 295.74,
+        "pressure": 1012,
+        "sea_level": 1012,
+        "grnd_level": 839,
+        "humidity": 47,
+        "temp_kf": 2.22
+      },
+      "weather": [
+        {
+          "id": 803,
+          "main": "Clouds",
+          "description": "broken clouds",
+          "icon": "04n"
+        }
+      ],
+      "clouds": {
+        "all": 78
+      },
+      "wind": {
+        "speed": 4.6,
+        "deg": 70,
+        "gust": 9.25
+      },
+      "visibility": 10000,
+      "pop": 0.01,
+      "sys": {
+        "pod": "n"
+      },
+      "dt_txt": "2026-02-19 21:00:00"
+    },
+    {
+      "dt": 1771545600,
+      "main": {
+        "temp": 290.73,
+        "feels_like": 290.24,
+        "temp_min": 290.73,
+        "temp_max": 290.73,
+        "pressure": 1014,
+        "sea_level": 1014,
+        "grnd_level": 837,
+        "humidity": 65,
+        "temp_kf": 0
+      },
+      "weather": [
+        {
+          "id": 804,
+          "main": "Clouds",
+          "description": "overcast clouds",
+          "icon": "04n"
+        }
+      ],
+      "clouds": {
+        "all": 96
+      },
+      "wind": {
+        "speed": 2.34,
+        "deg": 55,
+        "gust": 5.96
+      },
+      "visibility": 10000,
+      "pop": 0.01,
+      "sys": {
+        "pod": "n"
+      },
+      "dt_txt": "2026-02-20 00:00:00"
+    },
+    {
+      "dt": 1771556400,
+      "main": {
+        "temp": 289.26,
+        "feels_like": 288.81,
+        "temp_min": 289.26,
+        "temp_max": 289.26,
+        "pressure": 1016,
+        "sea_level": 1016,
+        "grnd_level": 838,
+        "humidity": 72,
+        "temp_kf": 0
+      },
+      "weather": [
+        {
+          "id": 804,
+          "main": "Clouds",
+          "description": "overcast clouds",
+          "icon": "04n"
+        }
+      ],
+      "clouds": {
+        "all": 86
+      },
+      "wind": {
+        "speed": 1.46,
+        "deg": 38,
+        "gust": 2.41
+      },
+      "visibility": 10000,
+      "pop": 0,
+      "sys": {
+        "pod": "n"
+      },
+      "dt_txt": "2026-02-20 03:00:00"
+    }
+  ],
+  "city": {
+    "id": 184745,
+    "name": "Nairobi",
+    "coord": {
+      "lat": -1.2921,
+      "lon": 36.8219
+    },
+    "country": "KE",
+    "population": 2750547,
+    "timezone": 10800,
+    "sunrise": 1771472538,
+    "sunset": 1771516269
+  }
+}
---
+++ Day 35 - Keys, Authentication & Environment Variables - Send SMS/notes.py
@@ -0,0 +1,80 @@
+# https://www.ventusky.com/thunderstorms-map/cape#p=-9.4;36.8;4
+# https://www.latlong.net/
+# https://www.pythonanywhere.com/
+
+import requests
+from twilio.rest import Client
+
+
+# Key authenticates your account to the OpenWeatherAPI provider
+openweather_api_key = "----------------------------------"
+twilio_account_sid = "----------------------------------"
+twilio_auth_token = "----------------------------------"
+
+# Python program that fetches the 5 day weather forecast of my location from the API url api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+# 
+# MY_LAT = -1.292066
+MY_LAT = 6.524379
+# MY_LONG = 36.821945
+MY_LONG = 3.379206
+
+API_URL = "https://api.openweathermap.org/data/2.5/forecast"
+
+# API params
+parameters = {
+    "lat": MY_LAT,
+    "lon": MY_LONG,
+    "appid": openweather_api_key,
+    "cnt": 4
+}
+
+# Fetch the data using get()
+response = requests.get(url=API_URL, params=parameters)
+# Manage the exceptions
+response.raise_for_status()
+# Extract the data
+weather_data = response.json()
+
+# Get the first weather id
+# print(weather_data["list"][0]["weather"][0]["id"])
+
+will_rain = False
+
+for hour_data in weather_data["list"]:
+    # print(hour_data["weather"][0]["id"])
+    condition_code = hour_data["weather"][0]["id"]
+
+    if int(condition_code) > 700:
+        # print("Bring an umbrella")
+        will_rain = True
+
+if will_rain:
+    print("Bring an umbrella")
+    # Create the twilio client
+    client = Client(twilio_account_sid, twilio_auth_token)
+
+    # Construct the message 
+    message = client.api.account.messages.create(
+        to="----------------------------------",
+        from_="----------------------------------",
+        body="It's going to rain today. Remember to carry an umbrella")
+    
+    print(message.sid)
+
+#---------- ENVIRONMENT VARIABLES ----------#
+''' 
+On Terminal, to see the environment variables:
+    - Windows -> dir Env:
+    - Linux -> env
+
+Are used for: 1. Convenience 
+              2. Security: Allows us to store keys and auth tokens in a separate file to the main code base
+
+Save an environment variable and use it in your Python code
+    On Terminal:
+        - Save environment variable -> export AUTH_TOKEN=<token_key> [AUTH_TOKEN is a variable name]
+    
+    In Python code:
+        - import os module
+        - Call token -> auth_token = os.environ.get("AUTH_TOKEN")
+'''
